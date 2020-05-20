3a8c5a278b0a9dbbd5126657d156d573b106175e
Adi <adrian.dan.busuioc@gmail.com>
2020-05-20 14:00:07 +0200

    GMS/HMS detection v1.0 ready

--- app/src/main/java/ch/busu/hmstests/MainActivity.kt
+++ app/src/main/java/ch/busu/hmstests/MainActivity.kt
@@ -1,12 +1,31 @@
 package ch.busu.hmstests
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import ch.busu.hmstests.databinding.ActivityMainBinding
+import com.google.android.gms.common.GoogleApiAvailability
+import com.huawei.hms.api.HuaweiApiAvailability
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+
+        binding.mainHasGMS.isChecked = isGmsAvailable(this)
+        binding.mainHasHMS.isChecked = isHmsAvailable(this)
+
+        setContentView(binding.root)
     }
+
+    private fun isGmsAvailable(context: Context): Boolean {
+        return GoogleApiAvailability.getInstance().isGooglePlayServicesAvailable(context) ==  com.google.android.gms.common.ConnectionResult.SUCCESS
+    }
+
+    private fun isHmsAvailable(context: Context): Boolean {
+        return HuaweiApiAvailability.getInstance().isHuaweiMobileServicesAvailable(context) == com.huawei.hms.api.ConnectionResult.SUCCESS
+    }
+
 }
