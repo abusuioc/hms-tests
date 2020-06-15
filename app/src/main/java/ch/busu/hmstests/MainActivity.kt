ba921f8a89fe4ea8d545d65549b9d5be555de73c
Adi <adrian.dan.busuioc@gmail.com>
2020-06-15 15:13:58 +0200

    Activity to demo the current location.

--- app/src/main/java/ch/busu/hmstests/MainActivity.kt
+++ app/src/main/java/ch/busu/hmstests/MainActivity.kt
@@ -1,11 +1,9 @@
 package ch.busu.hmstests
 
-import android.content.Context
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import ch.busu.hmstests.databinding.ActivityMainBinding
-import com.google.android.gms.common.GoogleApiAvailability
-import com.huawei.hms.api.HuaweiApiAvailability
+import mobileservices.detector.MobileServicesDetector
 
 class MainActivity : AppCompatActivity() {
 
@@ -14,18 +12,9 @@ class MainActivity : AppCompatActivity() {
 
         val binding = ActivityMainBinding.inflate(layoutInflater)
 
-        binding.mainHasGMS.isChecked = isGmsAvailable(this)
-        binding.mainHasHMS.isChecked = isHmsAvailable(this)
+        binding.mainHasGMS.isChecked = MobileServicesDetector.isGmsAvailable(this)
+        binding.mainHasHMS.isChecked = MobileServicesDetector.isHmsAvailable(this)
 
         setContentView(binding.root)
     }
-
-    private fun isGmsAvailable(context: Context): Boolean {
-        return GoogleApiAvailability.getInstance().isGooglePlayServicesAvailable(context) ==  com.google.android.gms.common.ConnectionResult.SUCCESS
-    }
-
-    private fun isHmsAvailable(context: Context): Boolean {
-        return HuaweiApiAvailability.getInstance().isHuaweiMobileServicesAvailable(context) == com.huawei.hms.api.ConnectionResult.SUCCESS
-    }
-
 }
